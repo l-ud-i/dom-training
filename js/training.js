923844862823278026ab0ec95ff717ffecf05cb9
Ludi <ludi.adressemail@gmail.com>
2023-04-26 15:09:45 +0200

    exo 1

--- js/training.js
+++ js/training.js
@@ -1,9 +1,25 @@
 /* ------------------------------------ */
 /* --- Exercice 1 --- */
+const disappear = document.querySelector("#ex1 .exercice-sandbox :nth-child(2)")
 
+function toggleDisplay(event) {
+    disappear.classList.toggle("js-text");
+    }
+    document.querySelector(".js-button").addEventListener('click', toggleDisplay);
+
+
+// const nav = document.querySelector("#ex1 .exercice-sandbox :nth-child(2)")
+// const nav2 = document.querySelector("#ex1 .exercice-sandbox :first-child")
+
+// nav2.addEventListener('click', function() {
+//     nav.classList.toggle("js-text");
+// });
 
 /* ------------------------------------ */
 /* --- Exercice 2 --- */
+
+const scrolled = document.querySelector("#ex2 .exercice-sandbox :first-child")
+console.log(scrolled);
 
 
 /* ------------------------------------ */
